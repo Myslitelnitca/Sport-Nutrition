b15c6e7a0cd5dc3382029fb1ff17e9c62488d7b1
Urruguay <myslitelnitca@gmail.com>
2024-12-12 02:47:57 +0200

    1-12practics
    Unfortunately, the project had to be redone and therefore immediately from 1 to 12 practice.

---
+++ SportNutrition.DAL/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace SportNutrition.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
